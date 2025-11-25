c698c649d491b60caf1db18f51356cb06153736f
bvanseg <bvanseg@gmail.com>
2025-11-25 18:06:54 -0500

    🚚 Migrated AlienItems to BLib registration.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/alien/common/registry/init/item/AlienItems.java
+++ common/src/main/java/com/alien/common/registry/init/item/AlienItems.java
@@ -1,11 +1,14 @@
 package com.alien.common.registry.init.item;
 
+import com.alien.Alien;
 import com.alien.AlienResources;
 import com.alien.common.gameplay.item.PoisonJellyItem;
 import com.alien.common.gameplay.item.RoyalJellyItem;
 import com.alien.common.registry.key.AlienJukeboxSongKeys;
 import com.avp.common.registry.AVPDeferredHolder;
 import com.avp.service.Services;
+import com.blib.BLibHolder;
+import com.blib.BLibRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.DiscFragmentItem;
 import net.minecraft.world.item.Item;
@@ -18,86 +21,100 @@ import java.util.function.Supplier;
 
 public class AlienItems {
 
+    public static final BLibRegistry<Item> REGISTRY = Alien.MOD.createRegistry(BuiltInRegistries.ITEM);
+
     private static final List<AVPDeferredHolder<? extends Item>> HOLDERS = new ArrayList<>();
 
     public static List<AVPDeferredHolder<? extends Item>> getAll() {
         return Collections.unmodifiableList(HOLDERS);
     }
 
-    public static final AVPDeferredHolder<Item> ABERRANT_CHITIN = register(
+    public static final BLibHolder<Item> ABERRANT_CHITIN = create(
         "aberrant_chitin",
         new Item.Properties().fireResistant()
     );
 
-    public static final AVPDeferredHolder<Item> ABERRANT_RESIN_BALL = register(
+    public static final BLibHolder<Item> ABERRANT_RESIN_BALL = create(
         "aberrant_resin_ball",
         new Item.Properties().fireResistant()
     );
 
-    public static final AVPDeferredHolder<Item> ALIEN_MUSIC_DISC_1 = register(
+    public static final BLibHolder<Item> ALIEN_MUSIC_DISC_1 = create(
         "alien_music_disc_1",
         new Item.Properties().stacksTo(1).rarity(Rarity.RARE).jukeboxPlayable(AlienJukeboxSongKeys.ALIEN_MUSIC_1)
     );
 
-    public static final AVPDeferredHolder<Item> ALIEN_MUSIC_DISC_1_FRAGMENT = register(
+    public static final BLibHolder<Item> ALIEN_MUSIC_DISC_1_FRAGMENT = create(
         "alien_music_disc_1_fragment",
         () -> new DiscFragmentItem(new Item.Properties())
     );
 
-    public static final AVPDeferredHolder<Item> CHITIN = register("chitin");
+    public static final BLibHolder<Item> CHITIN = create("chitin");
 
-    public static final AVPDeferredHolder<Item> IRRADIATED_CHITIN = register("irradiated_chitin");
+    public static final BLibHolder<Item> IRRADIATED_CHITIN = create("irradiated_chitin");
 
-    public static final AVPDeferredHolder<Item> IRRADIATED_RESIN_BALL = register("irradiated_resin_ball");
+    public static final BLibHolder<Item> IRRADIATED_RESIN_BALL = create("irradiated_resin_ball");
 
-    public static final AVPDeferredHolder<Item> NETHER_CHITIN = register("nether_chitin", new Item.Properties().fireResistant());
+    public static final BLibHolder<Item> NETHER_CHITIN = create("nether_chitin", new Item.Properties().fireResistant());
 
-    public static final AVPDeferredHolder<Item> NETHER_RESIN_BALL = register(
+    public static final BLibHolder<Item> NETHER_RESIN_BALL = create(
         "nether_resin_ball",
         new Item.Properties().fireResistant()
     );
 
-    public static final AVPDeferredHolder<Item> OVOID_POTTERY_SHERD = register("ovoid_pottery_sherd");
+    public static final BLibHolder<Item> OVOID_POTTERY_SHERD = create("ovoid_pottery_sherd");
 
-    public static final AVPDeferredHolder<Item> PARASITE_POTTERY_SHERD = register("parasite_pottery_sherd");
+    public static final BLibHolder<Item> PARASITE_POTTERY_SHERD = create("parasite_pottery_sherd");
 
-    public static final AVPDeferredHolder<Item> PLATED_CHITIN = register("plated_chitin");
+    public static final BLibHolder<Item> PLATED_CHITIN = create("plated_chitin");
 
-    public static final AVPDeferredHolder<Item> PLATED_ABERRANT_CHITIN = register(
+    public static final BLibHolder<Item> PLATED_ABERRANT_CHITIN = create(
         "plated_aberrant_chitin",
         new Item.Properties().fireResistant()
     );
 
-    public static final AVPDeferredHolder<Item> PLATED_IRRADIATED_CHITIN = register("plated_irradiated_chitin");
+    public static final BLibHolder<Item> PLATED_IRRADIATED_CHITIN = create("plated_irradiated_chitin");
 
-    public static final AVPDeferredHolder<Item> PLATED_NETHER_CHITIN = register(
+    public static final BLibHolder<Item> PLATED_NETHER_CHITIN = create(
         "plated_nether_chitin",
         new Item.Properties().fireResistant()
     );
 
-    public static final AVPDeferredHolder<Item> POISON_JELLY = register("poison_jelly", PoisonJellyItem::new);
+    public static final BLibHolder<Item> POISON_JELLY = create("poison_jelly", PoisonJellyItem::new);
 
-    public static final AVPDeferredHolder<Item> RAW_ROYAL_JELLY = register("raw_royal_jelly", RoyalJellyItem::new);
+    public static final BLibHolder<Item> RAW_ROYAL_JELLY = create("raw_royal_jelly", RoyalJellyItem::new);
 
-    public static final AVPDeferredHolder<Item> RESIN_BALL = register("resin_ball");
+    public static final BLibHolder<Item> RESIN_BALL = create("resin_ball");
 
-    public static final AVPDeferredHolder<Item> ROYALTY_POTTERY_SHERD = register("royalty_pottery_sherd");
+    public static final BLibHolder<Item> ROYALTY_POTTERY_SHERD = create("royalty_pottery_sherd");
 
-    public static final AVPDeferredHolder<Item> VECTOR_POTTERY_SHERD = register("vector_pottery_sherd");
+    public static final BLibHolder<Item> VECTOR_POTTERY_SHERD = create("vector_pottery_sherd");
 
-    public static AVPDeferredHolder<Item> register(String name) {
-        return register(name, new Item.Properties());
+    public static BLibHolder<Item> create(String name) {
+        return create(name, new Item.Properties());
     }
 
+    public static BLibHolder<Item> create(String name, Item.Properties properties) {
+        return create(name, () -> new Item(properties));
+    }
+
+    public static <T extends Item> BLibHolder<T> create(String name, Supplier<T> itemSupplier) {
+        return REGISTRY.createHolder(name, itemSupplier);
+    }
+
+    @Deprecated(forRemoval = true)
     public static AVPDeferredHolder<Item> register(String name, Item.Properties properties) {
         return register(name, () -> new Item(properties));
     }
 
+    @Deprecated(forRemoval = true)
     public static <T extends Item> AVPDeferredHolder<T> register(String name, Supplier<T> itemSupplier) {
         var holder = Services.REGISTRY.register(BuiltInRegistries.ITEM, AlienResources.location(name), itemSupplier);
         HOLDERS.add(holder);
         return holder;
     }
 
-    public static void initialize() {}
+    public static void initialize() {
+        REGISTRY.registerAll();
+    }
 }
