23ce7ee8fc96e787fbddad28f2d106b45a7f4ca5
bvanseg <bvanseg@gmail.com>
2025-11-26 18:59:52 -0500

    🐞 Added data migration for royal altar structures.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/alien/common/data/fixer/migration/impl/AVP_0_3_0_To_Alien_0_1_0.java
+++ common/src/main/java/com/alien/common/data/fixer/migration/impl/AVP_0_3_0_To_Alien_0_1_0.java
@@ -1,12 +1,17 @@
 package com.alien.common.data.fixer.migration.impl;
 
 import com.alien.Alien;
+import com.alien.AlienResources;
 import com.avp.AVPResources;
 import com.blib.common.data.fixer.BLibDataFixerRegistry;
 import com.blib.common.data.fixer.migration.BLibDataMigration;
 import com.lib.common.util.Version;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import org.jetbrains.annotations.NotNull;
 
 public class AVP_0_3_0_To_Alien_0_1_0 implements BLibDataMigration {
 
@@ -22,12 +27,30 @@ public class AVP_0_3_0_To_Alien_0_1_0 implements BLibDataMigration {
 
     @Override
     public void apply() {
+        registerTemplatePoolDataFixes();
         BuiltInRegistries.REGISTRY.forEach(AVP_0_3_0_To_Alien_0_1_0::registerMigrationsForRegistry);
+    }
+
+    private static void registerTemplatePoolDataFixes() {
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "badlands_royal_altar_main"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "badlands_royal_altar_egg"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "deepslate_royal_altar_main"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "deepslate_royal_altar_egg"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "desert_royal_altar_main"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "desert_royal_altar_egg"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "jungle_royal_altar_main"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "jungle_royal_altar_egg"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "nether_royal_altar_main"));
+        register(createResourceKeyEntry(Registries.TEMPLATE_POOL, "nether_royal_altar_egg"));
+    }
+
+    private static BLibDataFixerRegistry.Entry.@NotNull Resource createResourceKeyEntry(ResourceKey<Registry<StructureTemplatePool>> registry, String path) {
+        return new BLibDataFixerRegistry.Entry.Resource(registry, AVPResources.location(path), AlienResources.location(path));
     }
 
     private static void registerMigrationsForRegistry(Registry<?> registry) {
         Alien.MOD.getAllHolders(registry)
-            .forEach(holder -> register(new BLibDataFixerRegistry.Entry(registry, AVPResources.location(holder.getPath()), holder.getResourceLocation())));
+            .forEach(holder -> register(new BLibDataFixerRegistry.Entry.Direct(registry, AVPResources.location(holder.getPath()), holder.getResourceLocation())));
     }
 
     public static void register(BLibDataFixerRegistry.Entry entry) {
