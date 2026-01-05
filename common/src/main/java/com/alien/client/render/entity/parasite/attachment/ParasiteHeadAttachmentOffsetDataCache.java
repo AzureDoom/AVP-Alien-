4c5851d38468f4d951c6fb8f20f5fdbe47ef31d4
bvanseg <bvanseg@gmail.com>
2026-01-04 20:27:54 -0500

    ♻️ Refactor ParasiteHeadAttachmentOffsetDataCache.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/alien/client/render/entity/parasite/attachment/ParasiteHeadAttachmentOffsetDataCache.java
+++ common/src/main/java/com/alien/client/render/entity/parasite/attachment/ParasiteHeadAttachmentOffsetDataCache.java
@@ -1,6 +1,5 @@
 package com.alien.client.render.entity.parasite.attachment;
 
-import com.blib.client.BLibClientMod;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
@@ -19,26 +18,17 @@ public class ParasiteHeadAttachmentOffsetDataCache {
     }
 
     public static void put(
-        BLibClientMod mod,
         Supplier<? extends EntityType<?>> entityTypeSupplier,
         ParasiteHeadAttachmentOffsetData parasiteHeadAttachmentOffsetData
     ) {
-        mod.events()
-            .onClientSetup()
-            .register(() -> {
-                var entityType = entityTypeSupplier.get();
-                var resourceLocation = BuiltInRegistries.ENTITY_TYPE.getKey(entityType);
-
-                put(mod, resourceLocation, parasiteHeadAttachmentOffsetData);
-            });
+        put(entityTypeSupplier.get(), parasiteHeadAttachmentOffsetData);
     }
 
-    public static void put(BLibClientMod mod, EntityType<?> entityType, ParasiteHeadAttachmentOffsetData parasiteHeadAttachmentOffsetData) {
-        put(mod, BuiltInRegistries.ENTITY_TYPE.getKey(entityType), parasiteHeadAttachmentOffsetData);
+    public static void put(EntityType<?> entityType, ParasiteHeadAttachmentOffsetData parasiteHeadAttachmentOffsetData) {
+        put(BuiltInRegistries.ENTITY_TYPE.getKey(entityType), parasiteHeadAttachmentOffsetData);
     }
 
     public static void put(
-        BLibClientMod mod,
         ResourceLocation resourceLocation,
         ParasiteHeadAttachmentOffsetData parasiteHeadAttachmentOffsetData
     ) {
