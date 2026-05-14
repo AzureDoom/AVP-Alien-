88ca0b72ec44623a55b8d0a2d7711545b3887b5f
= <bvanseg@gmail.com>
2026-05-14 08:36:42 -0400

    Spawn reserve carriers with facehugger passengers

--- common/src/main/java/com/alien/common/gameplay/entity/living/alien/xenomorph/carrier/CarrierData.java
+++ common/src/main/java/com/alien/common/gameplay/entity/living/alien/xenomorph/carrier/CarrierData.java
@@ -8,11 +8,15 @@ public class CarrierData implements NBTSerializable {
 
     private static final String NBT_THROW_COOLDOWN_IN_TICKS = "throwCooldownInTicks";
 
+    private static final String NBT_RESERVE_FACEHUGGER_PAYLOAD_PENDING = "reserveFacehuggerPayloadPending";
+
     private static final int MIN_THROW_COOLDOWN_IN_TICKS = 20 * 5;
 
     private static final int MAX_THROW_COOLDOWN_IN_TICKS = 20 * 10;
 
     private int throwCooldownInTicks;
+
+    private boolean reserveFacehuggerPayloadPending;
 
     public void tick() {
         if (throwCooldownInTicks > 0) {
@@ -31,15 +35,25 @@ public class CarrierData implements NBTSerializable {
         );
     }
 
+    public boolean isReserveFacehuggerPayloadPending() {
+        return reserveFacehuggerPayloadPending;
+    }
+
+    public void setReserveFacehuggerPayloadPending(boolean reserveFacehuggerPayloadPending) {
+        this.reserveFacehuggerPayloadPending = reserveFacehuggerPayloadPending;
+    }
+
     @Override
     public void load(CompoundTag compoundTag) {
         if (compoundTag.contains(NBT_THROW_COOLDOWN_IN_TICKS)) {
             this.throwCooldownInTicks = compoundTag.getInt(NBT_THROW_COOLDOWN_IN_TICKS);
         }
+        this.reserveFacehuggerPayloadPending = compoundTag.getBoolean(NBT_RESERVE_FACEHUGGER_PAYLOAD_PENDING);
     }
 
     @Override
     public void save(CompoundTag compoundTag) {
         compoundTag.putInt(NBT_THROW_COOLDOWN_IN_TICKS, throwCooldownInTicks);
+        compoundTag.putBoolean(NBT_RESERVE_FACEHUGGER_PAYLOAD_PENDING, reserveFacehuggerPayloadPending);
     }
 }
