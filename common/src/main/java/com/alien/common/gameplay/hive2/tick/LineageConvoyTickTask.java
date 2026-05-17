c2823adf6ed858bc6e8af359f8ebd954cc698655
= <bvanseg@gmail.com>
2026-05-17 00:14:20 -0400

    Return raids from spectator targets

--- common/src/main/java/com/alien/common/gameplay/hive2/tick/LineageConvoyTickTask.java
+++ common/src/main/java/com/alien/common/gameplay/hive2/tick/LineageConvoyTickTask.java
@@ -197,7 +197,7 @@ public final class LineageConvoyTickTask {
 
     private static boolean shouldReturnHome(Convoy.Raid raid, MinecraftServer server) {
         var player = server.getPlayerList().getPlayer(raid.targetPlayerId());
-        return player != null && (!player.isAlive() || player.isCreative());
+        return player != null && (!player.isAlive() || player.isCreative() || player.isSpectator());
     }
 
     private static double materializedMemberLeashDistanceSqr(HiveConfig config) {
