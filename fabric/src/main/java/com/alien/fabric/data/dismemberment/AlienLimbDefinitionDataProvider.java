8a1ac4758ee85dd1de70c4517bbc3bd48d4a09a8
bvanseg <bvanseg@gmail.com>
2026-05-21 10:08:58 -0400

    ♻️ Moved all xeno limb data to datagen.
    Signed-off-by: = <bvanseg@gmail.com>

--- fabric/src/main/java/com/alien/fabric/data/dismemberment/AlienLimbDefinitionDataProvider.java
+++ fabric/src/main/java/com/alien/fabric/data/dismemberment/AlienLimbDefinitionDataProvider.java
@@ -1,7 +1,6 @@
 package com.alien.fabric.data.dismemberment;
 
 import com.alien.AlienResources;
-import com.alien.common.registry.init.AlienEntityTypes;
 import com.blib.api.common.dismemberment.v1.LimbCategories;
 import com.blib.api.common.dismemberment.v1.datagen.LimbDefinitionDataProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
@@ -9,29 +8,38 @@ import net.minecraft.resources.ResourceLocation;
 
 public final class AlienLimbDefinitionDataProvider extends LimbDefinitionDataProvider {
 
-    private static final ResourceLocation DRONE_TEMPLATE = AlienResources.location("drone_template");
-
     public AlienLimbDefinitionDataProvider(FabricDataOutput output) {
         super(output);
     }
 
     @Override
     protected void generate() {
-        template(DRONE_TEMPLATE)
-            .fatalLimb(limb("drone_head"), LimbCategories.HEAD)
-            .limb(limb("drone_left_arm"), LimbCategories.ARM)
-            .limb(limb("drone_right_arm"), LimbCategories.ARM)
-            .limb(limb("drone_left_leg"), LimbCategories.LEG)
-            .limb(limb("drone_right_leg"), LimbCategories.LEG)
-            .limb(limb("drone_tail"), LimbCategories.TAIL);
-
-        entity(AlienEntityTypes.DRONE).parent(DRONE_TEMPLATE);
-        entity(AlienEntityTypes.ABERRANT_DRONE).parent(DRONE_TEMPLATE);
-        entity(AlienEntityTypes.IRRADIATED_DRONE).parent(DRONE_TEMPLATE);
-        entity(AlienEntityTypes.NETHER_DRONE).parent(DRONE_TEMPLATE);
+        for (var group : AlienXenomorphLimbGroups.ALL) {
+            xenomorph(group);
+        }
     }
 
-    private static ResourceLocation limb(String path) {
-        return AlienResources.location(path);
+    private void xenomorph(AlienXenomorphLimbGroups.Group group) {
+        var prefix = group.prefix();
+        var parent = templateId(prefix);
+        template(parent)
+            .fatalLimb(limb(prefix, "head"), LimbCategories.HEAD)
+            .limb(limb(prefix, "left_arm"), LimbCategories.ARM)
+            .limb(limb(prefix, "right_arm"), LimbCategories.ARM)
+            .limb(limb(prefix, "left_leg"), LimbCategories.LEG)
+            .limb(limb(prefix, "right_leg"), LimbCategories.LEG)
+            .limb(limb(prefix, "tail"), LimbCategories.TAIL);
+
+        for (var entityType : group.entityTypes()) {
+            entity(entityType).parent(parent);
+        }
+    }
+
+    private static ResourceLocation templateId(String prefix) {
+        return AlienResources.location(prefix + "_template");
+    }
+
+    private static ResourceLocation limb(String prefix, String suffix) {
+        return AlienResources.location(prefix + "_" + suffix);
     }
 }
--- fabric/src/main/java/com/alien/fabric/data/dismemberment/AlienLimbVisualsDataProvider.java
+++ fabric/src/main/java/com/alien/fabric/data/dismemberment/AlienLimbVisualsDataProvider.java
@@ -1,14 +1,11 @@
 package com.alien.fabric.data.dismemberment;
 
 import com.alien.AlienResources;
-import com.alien.common.registry.init.AlienEntityTypes;
 import com.blib.api.common.dismemberment.v1.datagen.LimbVisualsDataProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.resources.ResourceLocation;
 
 public final class AlienLimbVisualsDataProvider extends LimbVisualsDataProvider {
-
-    private static final ResourceLocation DRONE_TEMPLATE = AlienResources.location("drone_template");
 
     public AlienLimbVisualsDataProvider(FabricDataOutput output) {
         super(output);
@@ -16,37 +13,59 @@ public final class AlienLimbVisualsDataProvider extends LimbVisualsDataProvider 
 
     @Override
     protected void generate() {
-        template(DRONE_TEMPLATE)
-            .visual(limb("drone_head"), "gHead", visuals -> visuals.renderOffset(0.0, 0.125, 0.0))
+        for (var group : AlienXenomorphLimbGroups.ALL) {
+            xenomorph(group);
+        }
+    }
+
+    private void xenomorph(AlienXenomorphLimbGroups.Group group) {
+        var prefix = group.prefix();
+        var headYOffset = group.queen() ? 0.375 : 0.125;
+        var headPitch = group.queen() ? -60.0 : 0.0;
+        var parent = templateId(prefix);
+        var file = template(parent);
+        file.visual(
+            limb(prefix, "head"),
+            "gHead",
+            visuals -> {
+                visuals.renderOffset(0.0, headYOffset, 0.0);
+                if (headPitch != 0.0) {
+                    visuals.renderRotation(headPitch, 0.0, 0.0);
+                }
+            }
+        )
             .visual(
-                limb("drone_left_arm"),
+                limb(prefix, "left_arm"),
                 "gLeftShoulder",
                 visuals -> visuals.renderOffset(0.0, 0.25, 0.0).renderRotation(-135.0, 0.0, 0.0)
             )
             .visual(
-                limb("drone_right_arm"),
+                limb(prefix, "right_arm"),
                 "gRightShoulder",
                 visuals -> visuals.renderOffset(0.0, 0.25, 0.0).renderRotation(-135.0, 0.0, 0.0)
             )
             .visual(
-                limb("drone_left_leg"),
+                limb(prefix, "left_leg"),
                 "gLeftLeg",
                 visuals -> visuals.renderOffset(0.0, 0.5, 0.0).renderRotation(-90.0, 0.0, 0.0)
             )
             .visual(
-                limb("drone_right_leg"),
+                limb(prefix, "right_leg"),
                 "gRightLeg",
                 visuals -> visuals.renderOffset(0.0, 0.5, 0.0).renderRotation(-90.0, 0.0, 0.0)
             )
-            .visual(limb("drone_tail"), "gTail1");
+            .visual(limb(prefix, "tail"), "gTail1");
 
-        entity(AlienEntityTypes.DRONE).parent(DRONE_TEMPLATE);
-        entity(AlienEntityTypes.ABERRANT_DRONE).parent(DRONE_TEMPLATE);
-        entity(AlienEntityTypes.IRRADIATED_DRONE).parent(DRONE_TEMPLATE);
-        entity(AlienEntityTypes.NETHER_DRONE).parent(DRONE_TEMPLATE);
+        for (var entityType : group.entityTypes()) {
+            entity(entityType).parent(parent);
+        }
     }
 
-    private static ResourceLocation limb(String path) {
-        return AlienResources.location(path);
+    private static ResourceLocation templateId(String prefix) {
+        return AlienResources.location(prefix + "_template");
+    }
+
+    private static ResourceLocation limb(String prefix, String suffix) {
+        return AlienResources.location(prefix + "_" + suffix);
     }
 }
