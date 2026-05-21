298927bb9abd5a24992436071ea68b9b4b4e26ed
bvanseg <bvanseg@gmail.com>
2026-05-21 19:24:39 -0400

    ♻️ Updated warrior model, animations and textures.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/alien/client/animation/entity/WarriorAnimator.java
+++ common/src/main/java/com/alien/client/animation/entity/WarriorAnimator.java
@@ -33,24 +33,6 @@ public class WarriorAnimator extends AzEntityAnimator<Warrior> {
         animationTrackContainer.add(
             AzAnimationTrack.builder(this, AzAlienAnimationUtil.BODY)
                 .setTransitionLength(5)
-                .build(),
-            AzAnimationTrack.builder(this, AzAlienAnimationUtil.HEAD)
-                .setTransitionLength(5)
-                .build(),
-            AzAnimationTrack.builder(this, AzAlienAnimationUtil.LEFT_ARM)
-                .setTransitionLength(5)
-                .build(),
-            AzAnimationTrack.builder(this, AzAlienAnimationUtil.LEFT_LEG)
-                .setTransitionLength(5)
-                .build(),
-            AzAnimationTrack.builder(this, AzAlienAnimationUtil.RIGHT_ARM)
-                .setTransitionLength(5)
-                .build(),
-            AzAnimationTrack.builder(this, AzAlienAnimationUtil.RIGHT_LEG)
-                .setTransitionLength(5)
-                .build(),
-            AzAnimationTrack.builder(this, AzAlienAnimationUtil.TAIL)
-                .setTransitionLength(5)
                 .build()
         );
     }
@@ -128,11 +110,11 @@ public class WarriorAnimator extends AzEntityAnimator<Warrior> {
         String animationName;
 
         if (attackType == Warrior.BITE) {
-            animationName = WarriorAnimationRefs.ATTACKBITE_HEAD_ANIMATION_NAME;
+            animationName = WarriorAnimationRefs.FULL_ATTACK_BITE_ANIMATION_NAME;
         } else if (attackType == Warrior.CLAW) {
-            animationName = WarriorAnimationRefs.ATTACKCLAW_RIGHTARM_ANIMATION_NAME;
+            animationName = WarriorAnimationRefs.FULL_ATTACK_CLAW_ANIMATION_NAME;
         } else if (attackType == Warrior.TAIL) {
-            animationName = WarriorAnimationRefs.ATTACKTAIL_TAIL_ANIMATION_NAME;
+            animationName = WarriorAnimationRefs.FULL_ATTACK_TAIL_ANIMATION_NAME;
         } else {
             animationName = null;
         }
